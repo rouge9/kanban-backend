d2f5947d0f0d4e9d735a40d6b71764060cee725a
rouge9 <robelged1992@gmail.com>
2026-08-19 14:56:49 +0300

    test: add initial unit tests for auth, users, and posts modules

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 describe('PostsController', () => {
   let controller: PostsController;
@@ -7,6 +8,9 @@ describe('PostsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
+      providers: [
+        { provide: PostsService, useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<PostsController>(PostsController);
--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsService } from './posts.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('PostsService', () => {
   let service: PostsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        { provide: PrismaService, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
